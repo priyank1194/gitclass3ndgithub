0985bf1e4cb5bb29f5374e6fd9f6482e813cbf43
priyank1194 <priyank9979561561@gmail.com>
2022-05-31 19:54:26 -0400

    2nd commit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -4,7 +4,7 @@ public class gitAndGithub {
 
 	public static void main(String[] args) {
 		System.out.println("We are learning git and github");
-
+		System.out.println("Today is a very good day");
 	}
 
 }
